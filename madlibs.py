d9ed334311b789155bbe00c1cd46e721bc679922
komisho <77800367+komisho@users.noreply.github.com>
2022-01-16 09:30:16 -0600

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+
+
+# Gather info
+noun1 = input("Give me a noun!")
+noun2 = input("Give me another noun!")
+noun3 = input("One more noun, please.")
+adj1 = input("Now give me an adjective.")
+verb1 = input("And finally, a verb.")
+madlib1 = f'My favorite breakfast consists of {noun1}, {noun2}, and {noun3}. I like to drown {noun3} in {adj1} and then {verb1} eat it all.'
+
+# Madlib
+print(madlib1)
